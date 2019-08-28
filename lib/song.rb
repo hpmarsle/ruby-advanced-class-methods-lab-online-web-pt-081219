f70179ef8d11be444c34908a036f750d9b093d4d
Mars Le <hp.mars.le@gmail.com>
2019-08-28 23:09:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,42 @@ class Song
 
   def save
     self.class.all << self
+    self
   end
-
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end 
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end  
+  
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save #why does this not work? without below code? isn't this saving the instantiated song to the @@all array??
+    #song (continued from above that did not work)
+    # all.find{|s|s.name == song_name}
+  end 
+  
+  def self.find_by_name(song_name)
+    all.find{|s|s.name == song_name}
+  end 
+  
+  def self.find_or_create_by_name(song_name)
+    if all.include?(find_by_name(song_name)) #if @@all array includes the instance of the object who has the song name 
+      find_by_name(song_name) #return that instance
+    else 
+      create_by_name(song_name) #otherwise create one
+    end 
+  end 
+  
+  def self.alphabetical
+    #
+  end 
 end
