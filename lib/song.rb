5591f4b640b836ac866a559643a62c3b3d30a144
Mars Le <hp.mars.le@gmail.com>
2019-08-28 23:48:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,8 +60,9 @@ class Song
     song 
   end 
   
-  def self.create_from_filename
-    
+  def self.create_from_filename(filename)
+    song = new_from_filename(filename)
+    song.save
   end 
   
   def self.destroy_all
